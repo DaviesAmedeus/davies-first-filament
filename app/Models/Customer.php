dfdd2f002ff3c49a19317016045ca91482a4d941
DaviesAmedeus <amedeusedgar@gmail.com>
2024-09-04 05:16:49 +0300

    Setting up, Customizing F.PHP theme, migrations & models & created a Product resource

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillablefillable = [
+        'name',
+        'email',
+        'phone',
+        'date_of_birth',
+        'address',
+        'zip_code',
+        'city'
+    ];
+
+}
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,6 @@
+
+<div class="flex gap-2">
+    <img src="{{ asset('images/d-logo.png') }}" alt="Davies Logo"
+    class="w-8"/>
+<div class="text-2xl">Davies Filament</div>    
+</div>
